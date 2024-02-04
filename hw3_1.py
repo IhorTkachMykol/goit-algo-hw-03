1046df133fb93182c7c10e77efeb1bb9c0e68069
IhorTkachMykol <igortkachmykol@gmail.com>
2024-02-04 18:09:19 +0200

    second correction

--- hw3_1.py
+++ hw3_1.py
@@ -1,14 +1,19 @@
 import datetime
-from datetime import datetime, timedelta
-today = datetime.today()
-today_date = today.date()
 
-certain_date_string = input("Введіть дату у форматі РРРР.ММ.ДД: ")
-certain_date_object = datetime.strptime(certain_date_string, "%Y.%m.%d")
-
-def get_days_from_today():
-    if today_date != certain_date_object:
-        difference = datetime.strptime(certain_date_string,"%Y.%m.%D") - datetime.today
-        print(difference.days)
+def get_days_from_today(today, certain_date_object):
+    if today != certain_date_object:
+        difference = certain_date_object - today
+        print("Різниця в днях:", difference.days)
     else:
-        print("Дата введена невірно")         
+        print("Введена дата співпадає з сьогоднішньою")
+
+try:
+    today = datetime.datetime.today().date()
+
+    certain_date_string = input("Введіть дату у форматі РРРР-ММ-ДД: ")
+    certain_date_object = datetime.datetime.strptime(certain_date_string, "%Y-%m-%d").date()
+
+    get_days_from_today(today, certain_date_object)
+
+except ValueError:
+    print("Неправильний формат дати. Введіть дату у форматі РРРР-ММ-ДД")
